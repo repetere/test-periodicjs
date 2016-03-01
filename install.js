bf4d4e94aa2cc2315f454a8ce1dd8662a9831a27
Yaw Joseph Etse <yaw.etse@gmail.com>
2016-02-29 19:20:35 -0500

    fixing post install

--- install.js
+++ install.js
@@ -1,154 +1,96 @@
-console.log('post install of test periodic');
-// /*
-//  * periodic
-//  * http://github.com/typesettin/periodic
-//  *
-//  * Copyright (c) 2014 Yaw Joseph Etse. All rights reserved.
-//  */
+/*
+ * periodic
+ * http://github.com/typesettin/periodic
+ *
+ * Copyright (c) 2014 Yaw Joseph Etse. All rights reserved.
+ */
 
-// 'use strict';
+'use strict';
+const Promisie = require('promisie');
+const fs = require('fs-extra');
+const path = require('path');
+const npm = require('npm');
+const asyncFs = Promisie.promisifyAll(fs);
+const node_module_to_test_and_copy = path.resolve(__dirname,'../../'); // /Users/yawetse/Developer/github/test/periodicjs.core.controller
+const name_of_module_to_copy = node_module_to_test_and_copy.split(path.sep)[node_module_to_test_and_copy.split(path.sep).length-1]; // periodicjs.core.controller
+const test_periodicjs_dir = path.resolve(node_module_to_test_and_copy,'node_modules/test-periodicjs/node_modules/periodicjs/'); // /Users/yawetse/Developer/github/test/periodicjs.core.controller/node_modules/test-periodicjs/node_modules/periodicjs
+const test_periodicjs_dir_node_modules_dir = path.resolve(test_periodicjs_dir,'node_modules'); // /Users/yawetse/Developer/github/test/periodicjs.core.controller/node_modules/test-periodicjs/node_modules/periodicjs/node_modules
+const copy_module_to_test_periodicjs_dir_extname = path.join(test_periodicjs_dir_node_modules_dir,name_of_module_to_copy); // /Users/yawetse/Developer/github/test/periodicjs.core.controller/node_modules/test-periodicjs/node_modules/periodicjs/node_modules/periodicjs.core.controller
 
-// var fs = require('fs-extra'),
-// 		path = require('path'),
-// 		async = require('async'),
-//  		Utilities = require('periodicjs.core.utilities'),
-// 		CoreUtilities = new Utilities({}),
-// 		npmhelper = require('./npmhelper')({}),
-// 		extensionsConfigPath = path.join(process.cwd(),'/content/config/extensions.json'),
-// 		applicationConfigPath = path.join(process.cwd(),'/content/config/config.json'),
-// 		appContentPath = path.join(process.cwd(),'/content/'),	
-// 		appContentPathBackupDir = path.join(process.cwd(),'../periodic_content_backup/'),
-// 		publicPath = path.join(process.cwd(),'/public/'),	
-// 		publicPathBackupDir = path.join(process.cwd(),'../periodic_public_backup/'),
-// 		customFilesPath = path.join(process.cwd()),	
-// 		customFilesPathBackupDir = path.join(process.cwd(),'../periodic_customfiles_backup/'),
-// 		extensionConfig,
-// 		applicationConfig,
-// 		backupDir,
-// 		alreadyInstalled= false;
+console.log('node_module_to_test_and_copy',node_module_to_test_and_copy); // /Users/yawetse/Developer/github/test/periodicjs.core.controller
+console.log('test_periodicjs_dir',test_periodicjs_dir); // /Users/yawetse/Developer/github/test/periodicjs.core.controller/node_modules/test-periodicjs/node_modules/periodicjs
+console.log('name_of_module_to_copy',name_of_module_to_copy); // periodicjs.core.controller
+console.log('test_periodicjs_dir_node_modules_dir',test_periodicjs_dir_node_modules_dir); // /Users/yawetse/Developer/github/test/periodicjs.core.controller/node_modules/test-periodicjs/node_modules/periodicjs/node_modules
+console.log('copy_module_to_test_periodicjs_dir_extname',copy_module_to_test_periodicjs_dir_extname); // /Users/yawetse/Developer/github/test/periodicjs.core.controller/node_modules/test-periodicjs/node_modules/periodicjs/node_modules/periodicjs.core.controller
 
 
-
-// // console.log('install.js process.cwd()',process.cwd());
-// // console.log('install.js __dirname',__dirname);
-// // console.log('install.js extensionsConfigPath',extensionsConfigPath);
-// // console.log('install.js applicationConfigPath',applicationConfigPath);
-// // console.log('install.js appContentPath',appContentPath);
-// // console.log('install.js appContentPathBackupDir',appContentPathBackupDir);
-// // console.log('install.js publicPath',publicPath);
-// // console.log('install.js publicPathBackupDir',publicPathBackupDir);
-// try{
-// 	backupDir = fs.statSync(publicPathBackupDir);
-// 	alreadyInstalled = true;
-// }
-// catch(e){
-// 	console.log('there is no backup dir',publicPathBackupDir);
-// }
-
-// try{
-// 	extensionConfig = fs.readJsonSync(extensionsConfigPath);
-// 	applicationConfig = fs.readJsonSync(applicationConfigPath);
-// 	if(alreadyInstalled || extensionConfig.extensions.length>0){
-// 	 	alreadyInstalled=true;
-// 	}
-// 	else if(alreadyInstalled ||applicationConfig.status==='active'){
-// 	 	alreadyInstalled=true;
-// 	}
-// }
-// catch(e){
-//  	alreadyInstalled=false;
-// }
-
-// var restoreExtensionBackup = function(){
-// 	async.parallel({
-// 			copy_content:	function(asyncCB){
-// 				fs.copy( appContentPathBackupDir,appContentPath,function(err){
-// 					if(err){
-// 						asyncCB(err);
-// 					}
-// 					else{
-// 						fs.remove(appContentPathBackupDir,asyncCB);
-// 					}
-// 				});
-// 			},
-// 			copy_public:	function(asyncCB){
-// 				fs.copy(publicPathBackupDir,publicPath, function(err){
-// 					if(err){
-// 						asyncCB(err);
-// 					}
-// 					else{
-// 						fs.remove(publicPathBackupDir,asyncCB);
-// 					}
-// 				});
-// 			},
-// 			copy_custom_files:	function(asyncCB){
-// 				fs.copy(customFilesPathBackupDir,customFilesPath, function(err){
-// 					if(err){
-// 						asyncCB(err);
-// 					}
-// 					else{
-// 						fs.remove(customFilesPathBackupDir,asyncCB);
-// 					}
-// 				});
-// 			}
-// 		},function(err,result){
-// 		  if (err){
-// 				console.log('copying appContentPathBackupDir & publicPathBackupDir backup error (directories do not exist)',err.message);
-// 			}
-// 			else{
-// 				console.log('successful appContentPathBackupDir & publicPathBackupDir backup',result);
-// 			}
-// 			process.exit(0);
-// 	});
-// };
-
-// console.log('alreadyInstalled',alreadyInstalled);
-// if(process.env.npm_config_skip_post_install){
-// 	console.log('skip post install');
-// 	restoreExtensionBackup();
-// }
-// else if(alreadyInstalled){
-// 	console.log('Periodicjs Already Installed, Upgrade Complete');
-// 	restoreExtensionBackup();
-// 	//deploy sync
-// 	async.waterfall([
-// 		npmhelper.getInstalledExtensions,
-// 		npmhelper.getMissingExtensionsFromConfig,
-// 		npmhelper.installMissingExtensions,
-// 		npmhelper.installMissingNodeModules,
-// 		npmhelper.getThemeName,
-// 		npmhelper.installThemeModules
-// 		],
-// 		function(err,result){	
-// 			if(err){
-// 				throw new Error(err);
-// 			}
-// 			else{
-// 				console.log('post install deploysync result',result);
-// 				CoreUtilities.restart_app({});
-// 			}
-// 	});
-// }
-// else {
-// 	fs.copy(path.join(__dirname,'../.npmignore'), path.join(__dirname,'../.gitignore'), function (err) {
-// 	  if (err) {return console.error(err);}
-// 	  console.log('copied ignore file!');
-// 	}); // copies file
-// 	npmhelper.cleanInstallStandardExtensions(
-// 		{},
-// 		function (err) {
-// 			if (err) {
-// 				console.error(err);
-// 				process.exit(0);
-// 			}
-// 			else {
-// 				console.log('Upgraded Periodicjs');
-// 				process.exit(0);
-// 			}
-// 		});	
-// }
-// fs.ensureDir(path.join(__dirname,'../logs'), function (err) {
-// 	if(err){
-// 	  console.log('creating log directory err',err); // => null
-// 	}
-// });
+asyncFs.ensureDirAsync(test_periodicjs_dir_node_modules_dir)
+	.then(()=>{
+		console.log('created test dir');
+		return asyncFs.copy(node_module_to_test_and_copy,copy_module_to_test_periodicjs_dir_extname,{
+			filter:function(file){
+				// console.log('file',file);
+				if(file.match(/node_modules\/test-periodicjs/gi)){
+					return false;
+				}
+				else{
+					return true;
+				}
+			}
+		});
+	})
+	.then(()=>{
+		try{
+			let extConfigExists = fs.statSync(path.join(node_module_to_test_and_copy,'periodicjs.ext.json'));
+			console.log('extConfigExists',extConfigExists);
+			if(!extConfigExists){
+				console.log('testing core module',extConfigExists);
+			}
+			else{
+				console.log('testing extension',extConfigExists);
+				return asyncFs.readJsonAsync(path.join(node_module_to_test_and_copy,'periodicjs.ext.json'));
+			}
+		}
+		catch(e){
+			console.log('testing core module');
+		}
+	})
+	.then((extensionDepJson)=>{
+		let extensionsToInstall=[];
+		if(extensionDepJson && Array.isArray(extensionDepJson.periodicDependencies) && extensionDepJson.periodicDependencies.length>0){
+			extensionDepJson.periodicDependencies.forEach(function(extObj){
+				if(extObj && extObj.extname && extObj.version){
+					extensionsToInstall.push( extObj.extname+'@'+extObj.version);
+				}
+			});
+		}
+		var npmconfig = {
+			'strict-ssl': false,
+			'save-optional': false,
+			'production': true,
+			'prefix': test_periodicjs_dir_node_modules_dir
+		};
+		npm.load(
+			npmconfig,
+			function (err) {
+			if (err) {
+				console.error(err);
+				throw err;
+			}
+			else {
+			 	// npm['save-optional'] = true;
+				npm.commands.install(
+					extensionsToInstall,
+					function(err,npmstatus){
+						console.error(err);
+						console.log('npmstatus',npmstatus);
+					}
+				);
+			}
+		});
+		// console.log('extensionsToInstall',extensionsToInstall);
+	})
+	.catch((err)=>{
+		console.log('test-periodicjs post install error',err);
+		process.exit(0);
+	});
